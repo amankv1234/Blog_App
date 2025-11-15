04a948edfcca66c1d9c66cce86f2f9d9a04f5a9e
Aman kumar vishwakrma <186990161+amankv1234@users.noreply.github.com>
2025-11-16 00:53:55 +0530

    Initial commit

---
+++ models/blog.js
@@ -0,0 +1,25 @@
+const {Schema,model}=require("mongoose");
+
+const blogSchema=new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    body:{
+        type:String,
+        required:true
+    },
+    coverImageURL:{
+        type:String,
+        required:false
+    },
+    createdBy:{
+        type:Schema.Types.ObjectId,
+        ref:"user"
+    },
+},
+{ timeseries:true }
+);
+
+const User=model('blog',blogSchema);
+module.exports=User;
